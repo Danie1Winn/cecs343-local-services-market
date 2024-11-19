1272e9ed64adcb9a1652a5c9a5a522770a4b42a3
John Le <jjuniorin@gmail.com>
2024-11-19 10:34:21 -0800

    Added profile picture for workers

--- app/static/js/auth.js
+++ app/static/js/auth.js
@@ -16,3 +16,33 @@ function showTab(tabName) {
         employerTab.classList.remove('active');
     }
 }
+
+function showImage(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const img = document.getElementById('profile-pic');
+            img.src = e.target.result;
+            img.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+    }
+}
+
+function validateForm() {
+    const profilePicture = document.getElementById("profile-picture");
+    const profileError = document.getElementById("profile-error");
+    const password = document.getElementById("password");
+    const rePassword = document.getElementById("re-password");
+    if (!profilePicture.files.length) {
+        profileError.style.display = "block";
+        return false;
+    }
+    if(password != rePassword) {
+        return false;
+    }
+    profileError.style.display = "none";
+    return true;
+
+}
