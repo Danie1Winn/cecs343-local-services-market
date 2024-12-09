00b41f59a3e7cb7e89e90b38771f96ebd67c1d23
John Le <jjuniorin@gmail.com>
2024-12-09 12:30:10 -0800

    Signin/login works with ATT verification and merge

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+import subprocess
 
 # Initialize db and migrate globally
 db = SQLAlchemy()
